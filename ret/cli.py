e0e9c9251a7b21a0c4abb7844e493317b52dc380
Abdun Nihaal <abdun.nihaal@gmail.com>
2022-03-09 10:02:48 +0530

    Select all benchmarks if benchmarks is not specified

--- ret/cli.py
+++ ret/cli.py
@@ -2,11 +2,13 @@ import click
 import yaml
 
 @click.group()
-def cli():
+@click.pass_context
+def cli(ctx):
+    ctx.ensure_object(dict)
     # Read and parse retconfig.yml from current directory
     with open("retconfig.yml", 'r') as configfile:
         try:
-            config = yaml.safe_load(configfile)
+            ctx.obj['config'] = yaml.safe_load(configfile)
         except yaml.YAMLError as err:
             print(err)
             print("Error in parsing retconfig.yml")
@@ -15,18 +17,20 @@ def cli():
 @cli.command()
 @click.option("--benchmarks", "-b", help="Comma separated list of benchmarks to run")
 @click.option("--models", "-m", required=True, help="Comma separated list of benchmarks to run")
-def run(benchmarks, models):
+@click.pass_context
+def run(ctx, benchmarks, models):
     print("Models to run    : ",end='')
     for model in models.split(","):
         print(f"{model} ", end='')
     print()
+
     if benchmarks:
-        print("Benchmarks to run: ",end='')
-        for benchmark in benchmarks.split(","):
-            print(f"{benchmark} ", end='')
-        print()
+        benchmarks = benchmarks.split(",")
     else:
-        # FIXME Run all benchmarks if none specified
-        pass
+        benchmarks = ctx.obj['config']['benchmarks']
+    print("Benchmarks to run: ",end='')
+    for benchmark in benchmarks:
+        print(f"{benchmark} ", end='')
+    print()
 
 cli()
