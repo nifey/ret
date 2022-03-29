551215147d60ea293c1b1ac645f689ad76af1682
Abdun Nihaal <abdun.nihaal@gmail.com>
2022-03-29 12:00:13 +0530

    Add plot command with basic barplot

--- ret/cli.py
+++ ret/cli.py
@@ -2,6 +2,7 @@ import click
 import os
 import subprocess
 import yaml
+import plot as plt
 
 from functools import partial
 from concurrent.futures import ProcessPoolExecutor
@@ -19,7 +20,7 @@ def cli(ctx):
             print("Error in parsing retconfig.yml")
             exit(0)
 
-def run_hook(config, hook_name, arguments):
+def run_hook(config, hook_name, arguments, capture_output=False):
     # Check if hook exists in config and in filesystem
     if hook_name not in config['hooks']:
         return
@@ -30,10 +31,13 @@ def run_hook(config, hook_name, arguments):
     # Run hook and retrieve return value
     command = [script]
     command.extend(arguments)
-    returncode = subprocess.run(command).returncode
+    completed_process = subprocess.run(command, capture_output=capture_output)
+    returncode = completed_process.returncode
     if returncode != 0:
         print(f"{hook_name} hook failed with error code {returncode}")
         exit(0)
+    if capture_output:
+        return str(completed_process.stdout, 'UTF-8')
 
 def execute_run(benchmark, config, model_name, data_dir):
     # Create a folder for this run
@@ -71,4 +75,44 @@ def run(ctx, benchmarks, model_name):
 
     run_hook(config, 'post_batch', [model_name, data_dir])
 
+@cli.command()
+@click.option("--benchmarks", "-b", help="Comma separated list of benchmarks to plot")
+@click.option("--models", "-m", required=True, help="Comma separated list of models to plot")
+@click.option("--metrics", "-M", required=True, help="Comma separated list of metrics to plot")
+@click.pass_context
+def plot(ctx, benchmarks, models, metrics):
+    config = ctx.obj['config']
+    data_dir = config['data_dir']
+
+    if benchmarks:
+        benchmarks = benchmarks.split(",")
+    else:
+        benchmarks = config['benchmarks']
+    print("Benchmarks to plot: ",end='')
+    for benchmark in benchmarks:
+        print(f"{benchmark} ", end='')
+    print()
+    models = models.split(",")
+    print("Models to plot: ",end='')
+    for model in models:
+        print(f"{model} ", end='')
+    print()
+    metrics = metrics.split(",")
+    print("Metrics to plot: ",end='')
+    for metric in metrics:
+        print(f"{metric} ", end='')
+    print()
+
+    for metric in metrics:
+        # Collect data for metric
+        plot_data = {}
+        for model in models:
+            plot_data[model] = []
+            for benchmark in benchmarks:
+                plot_data[model].append(0)
+            for benchmark in benchmarks:
+                run_dir = os.path.join(os.getcwd(), data_dir, f"{model}_{benchmark}")
+                data = run_hook(config, 'get_metric', [model, benchmark, run_dir, metric], capture_output=True)
+                plot_data[model][benchmarks.index(benchmark)] = float(data)
+        plt.bar_plot(plot_data, benchmarks, title=metric, gmean=True)
 cli()
---
+++ ret/plot.py
@@ -0,0 +1,45 @@
+import functools
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+
+def calc_gmean(numbers):
+    sum_of_logs = functools.reduce(lambda x, y: x+y,
+                              map(math.log, numbers))
+    return pow(math.e, sum_of_logs/len(numbers))
+
+def bar_plot(data, xticks, title=None, filename=None, gmean=False):
+    """Create a Bar plot
+
+    Takes as argument a dict from (model => [model_value for each benchmark])
+    """
+    if gmean:
+        for model in data.keys():
+            data[model].append(calc_gmean(data[model]))
+
+    total_width = 0.8
+    bar_width = total_width/len(data)
+    for i, model in enumerate(data.keys()):
+        current_plot_data = data[model]
+        start_offset = (total_width/2) - (2*i+1)*bar_width/2
+        plt.bar(np.arange(len(current_plot_data)) - start_offset, current_plot_data, width=bar_width, label=model)
+    plt.title(title or "Bar chart")
+    xticks = xticks[:]
+    if gmean:
+        xticks.append("gmean")
+    plt.xticks(np.arange(len(xticks)), xticks)
+    plt.legend()
+    #plt.savefig(filename or f"plots/{title}.jpg")
+    plt.show()
+
+def stacked_bar_plot():
+    pass
+
+def violin_plot(data, title=None, xticks=None, filename=None):
+    plt.violinplot(data, showmeans=True)
+    plt.title(title or "Violin Plot")
+    if xticks:
+        plt.xticks(np.arange(len(xticks))+1, xticks)
+    plt.legend()
+    plt.savefig(f"plots/{title}.jpg")
+    plt.show()
